f59c523fe4ff258b23fd54ed8198bb47892fdbaa
Durga Sumesh <118443621+aaagrud@users.noreply.github.com>
2024-02-14 22:29:50 +0530

    Create student.cpp

---
+++ student.cpp
@@ -0,0 +1,3 @@
+/*
+Write program to create class student with age, number and register number. Using inheritance, derive two classes M.Tech and B.Tech student. List both catagory of student in the increasing order of marks (B.Tech) and gpa (M.Tech). In case of tie, display whichever name comes first. Make sort function virtual.
+*/
